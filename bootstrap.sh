85054b826a6d6469d04f061c3e2acecaa245f223
tobias <tobiasleongzhunmun@gmail.com>
2019-02-17 22:19:27 +0800

    add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+sudo su -
+yum update -y
+
+yum install -y git
+yum install -y python3
+
+mkdir -p /var/scrapper
+cd /var/scrapper
+git clone https://github.com/jaabberwocky/hwz-textgenrnn .
+
+pip3 install virtualenv
+virtualenv venv
+source venv/bin/activate
+pip3 install -r requirements.textgenrnn
+
