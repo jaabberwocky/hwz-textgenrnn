267990098e7b21d7b476647fb778cde9751b80f0
tobias <tobiasleongzhunmun@gmail.com>
2019-02-20 23:24:00 +0800

    add command line arg for num_epochs for train.py

--- textgenerator/train.py
+++ textgenerator/train.py
@@ -2,13 +2,14 @@ from textgenrnn import textgenrnn
 import sys
 import os
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 3:
     raise Exception("Incorrect number of commandline arguments.")
 else:
     trainFilePath = sys.argv[1]
+    num_epochs = sys.argv[2]
 
 textgen = textgenrnn()
-textgen.train_from_file(trainFilePath, num_epochs = 1, verbose = 1)
+textgen.train_from_file(trainFilePath, num_epochs = num_epochs, verbose = 1)
 if os.path.isfile('textgenmodel_saved.hdf5'):
     os.remove('textgenmodel_saved.hdf5')
 textgen.save('textgenmodel_saved.hdf5')
